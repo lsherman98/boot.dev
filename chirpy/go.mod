25a71e31aac288f158149940e3f3a3dd20441aa3
lsherman98 <122409681+lsherman98@users.noreply.github.com>
2024-07-22 23:17:38 -0400

    go servers

---
+++ chirpy/go.mod
@@ -0,0 +1,3 @@
+module github.com/lsherman98/boot.dev/chirpy
+
+go 1.22.1
---
+++ chirpy/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+    filepathRoot := "."
+    port := "8080"
+    
+    mux := http.NewServeMux()
+    mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+    mux.HandleFunc("/healthz", handlerReadiness)
+
+    srv:= http.Server{
+        Addr: ":" + port,
+        Handler: mux,
+    }
+    
+    log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+    log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(http.StatusText(http.StatusOK)))
+}
